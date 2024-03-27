8f7739c9191e6bc3f9fa1b8a3d8ece1f9c9a321a
bmck039 <mckallipb@southwestern.edu>
2024-03-27 16:12:23 -0500

    renamed to expectiminimax for clearer naming

--- expectiminimax.py
+++ expectiminimax.py
@@ -2,18 +2,20 @@ import random
 
 from util import Card
 from util import Suit
+from players import PlayingClass
 
 import stable_baselines3
 
-class CardPlayingAgent():
+class ExpectiMiniMax(PlayingClass):
     def __init__(self) -> None:
         super()
 
     def evaluateMove(discardPile: list[Card], move: Card):
         discardPile += [move]
+        # TODO
 
-        # TODO: figure out how pytorch works and implement some sort of reinforcement learning
-
+    def play(rules, hand, state): #returns a valid move
+        pass
 
 
     # def updateModel(state, action, reward):
