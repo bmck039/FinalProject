476229bbe85afd3d88e1a1a58051f8180b023d72
Matthew Volkin <119533382+MattVolkin@users.noreply.github.com>
2024-03-27 16:54:11 -0500

    very basic eval fuction complete

--- expectiminimax.py
+++ expectiminimax.py
@@ -4,16 +4,26 @@ from util import Card
 from util import Suit
 from players import PlayingClass
 
+
 import stable_baselines3
 
 class ExpectiMiniMax(PlayingClass):
     def __init__(self) -> None:
         super()
 
-    def evaluateMove(discardPile: list[Card], move: Card):
-        discardPile += [move]
-        # TODO
+    def evaluateMove(discardPile: list[Card], move: Card,rules,state,player):
+        for card in discardPile:
+          result =   rules.compareCard(move,card) # if card does not win trick it is a bad move
+          if result != 1:
+              return -1
+        if state["bids"][player]  >= state["tricks"][player]+1 : # if winning this trick puts the player over the bid number this is a bad move
+            return 1 # good move  otherwise
+        else:
+            return -1
 
+            
+    
+        # TODO
     def play(rules, hand, state): #returns a valid move
         pass
 
