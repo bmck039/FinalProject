31aed18e77f04bb14b7126b9be75c2c095711aa4
bmck039 <mckallipb@southwestern.edu>
2024-03-27 16:26:30 -0500

    fixed a circular import

--- play.py
+++ play.py
@@ -3,13 +3,9 @@ import players
 
 import random
 
-def randomMove(rules, hand, state):
-    moves = rules.validMoves(hand, state)
-    return random.choice(moves)
-
 playersList = []
 for i in range(4):
-    playersList.append(players.AIPlayer(randomMove))
+    playersList.append(players.AIPlayer(players.RandomPlay))
 
 #creates a game with 4 AI players
 
