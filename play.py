231a75230b1905c861562bcd906caa8464409fce
Desktop <mckallipb@southwestern.edu>
2024-04-17 15:04:09 -0500

    created tournament

--- play.py
+++ play.py
@@ -1,18 +1,30 @@
 import util
 import players
 import MCTS
-import expectiminimax
-import RL
+# import expectiminimax
+# import RL
+
+import time
 
 def play(rules, playerList):
+    start = time.time()
     game = util.Game(rules, playerList)
-    print(game.state["bids"])
     game.playUntilWin()
     # game.playTurn()
-    print(game.state["scores"])
+    score = game.state["scores"]
+    end = time.time()
+    print(f"round finished in {end - start:.6f} seconds")
+    filename = playerList[0].playingClass.__name__ + "And" + playerList[2].playingClass.__name__ + "VS" + playerList[1].playingClass.__name__ + "And" + playerList[3].playingClass.__name__
+    with open(filename + ".csv", "a+") as file:
+        if(score[1] > score[0]):
+            winner = 1
+        else: winner = 0
+        scoreDifference = score[1] - score[0]
+        file.write(str(winner) + "," + str(scoreDifference) + "\n")
 
 def tournament(rules, playerList, rounds):
     for i in range(rounds):
+        print("running round", i)
         play(rules, playerList)
 
 playersList = []
@@ -22,4 +34,5 @@ playersList.append(players.AIPlayer(players.RandomPlay))
 playersList.append(players.AIPlayer(MCTS.MCTSPlay))
 #creates a game with 4 AI players
 
-play(util.Spades, playersList)
+tournament(util.Spades, playersList, 600)
+# play(util.Spades, playersList)
