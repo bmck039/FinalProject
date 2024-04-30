1ac0de675f924d764cb02430e4b4134fd2671efa
bmck039 <mckallipb@southwestern.edu>
2024-04-30 16:37:15 -0500

    automatic file updating

--- updateFiles.ps1
+++ updateFiles.ps1
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-strip-hints spades.py -o SIMPLE/app/environments/spades/spades/envs/spades.py
-strip-hints probabilities.py -o SIMPLE/app/environments/spades/spades/envs/probabilities.py
-cp "probabilities.json" "SIMPLE/app/environments/spades/spades/envs/probabilities.json"
-strip-hints util.py -o SIMPLE/app/environments/spades/spades/envs/util.py
+# strip-hints spades.py -o SIMPLE/app/environments/spades/spades/envs/spades.py
+# strip-hints probabilities.py -o SIMPLE/app/environments/spades/spades/envs/probabilities.py
+# cp "probabilities.json" "SIMPLE/app/environments/spades/spades/envs/probabilities.json"
+# strip-hints util.py -o SIMPLE/app/environments/spades/spades/envs/util.py
 
 strip-hints play.py -o SIMPLE/app/play.py
 strip-hints players.py -o SIMPLE/app/players.py
@@ -12,4 +12,4 @@ strip-hints RL.py -o SIMPLE/app/RL.py
 strip-hints util.py -o SIMPLE/app/util.py
 strip-hints probabilities.py -o SIMPLE/app/probabilities.py
 cp "probabilities.json" "SIMPLE/app/probabilities.json"
-strip-hints spades.py -o SIMPLE/app/spades.py
+strip-hints spades.py -o SIMPLE/app/spadesEnv.py
